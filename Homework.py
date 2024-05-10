97145418524e25fe22d750749b18bf3798d36ab2
user <andrey.gromov.36912@gmail.com>
2024-05-10 22:05:57 +0300

    Завдання 4

--- Homework.py
+++ Homework.py
@@ -1,59 +1,43 @@
-# Завдання 4
-# Користувач вводить з клавіатури шлях до існуючої директорії та слово для пошуку. Після чого запускаються два
-# потоки. Перший потік має знайти файли з потрібним словом
-# і злити їх вміст в один файл. Другий потік очікує на завершення роботи першого потоку і проводить виключення усіх
-# заборонених слів (список цих слів потрібно зчитати з файлу
-# із забороненими словами) з отриманого файлу. Виведіть статистику виконаних операцій на екран.
+# Завдання 1
+# При старті додатку запускаються три потоки. Перший
+# потік заповнює список випадковими числами. Два інші потоки
+# очікують на заповнення. Коли перелік заповнений, обидва
+# потоки запускаються. Перший потік знаходить суму елементів
+# списку, другий потік знаходить середнє арифметичне значення
+# у списку. Отриманий список, сума та середнє арифметичне
+# виводяться на екран.
 
-import os
 import threading
-source_dir = input("Введіть шлях до директорії: ")
-search_word = input("Введіть слово для пошуку: ")
-banned_words_file = input("Введіть шлях до файлу зі збороненими словами: ")
-def search_and_merge_files(source_dir, search_word, output_file):
-    try:
-        matching_files = []
-        for root, _, files in os.walk(source_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                if os.path.isfile(file_path):
-                    with open(file_path, 'r') as file:
-                        if search_word in file.read():
-                            matching_files.append(file_path)
-        with open(output_file, 'w') as out_file:
-            for file_path in matching_files:
-                with open(file_path, 'r') as in_file:
-                    out_file.write(in_file.read() + '\n')
+import random
+numbers = []
 
-        print(f"Знайдено та злито {len(matching_files)} файлів з потрібним словом.")
-    except Exception as e:
-        print(f"Під час пошуку та злиття файлів виникла помилка: {e}")
+def fill_random_numbers(nums):
+    for _ in range(10):
+        nums.append(random.randint(1, 100))
+    print("Отриманий список:", nums)
+def find_sum(nums):
+    sum_result = sum(nums)
+    print("Сума елементів списку:", sum_result)
+def find_average(nums):
+    if nums:
+        average = sum(nums) / len(nums)
+        print("Середнє арифметичне значення у списку:", average)
+    else:
+        print("Список пустий.")
 
-def exclude_banned_words(input_file, banned_words_file, output_file):
-    try:
 
-        with open(banned_words_file, 'r') as file:
-            banned_words = set(file.read().splitlines())
 
-        with open(input_file, 'r') as in_file:
-            content = in_file.read()
-        for banned_word in banned_words:
-            content = content.replace(banned_word, '')
+fill_thread = threading.Thread(target=fill_random_numbers, args=(numbers,))
+sum_thread = threading.Thread(target=find_sum, args=(numbers,))
+average_thread = threading.Thread(target=find_average, args=(numbers,))
+fill_thread.start()
+fill_thread.join()
+sum_thread.start()
+average_thread.start()
+sum_thread.join()
+average_thread.join()
 
-        with open(output_file, 'w') as out_file:
-            out_file.write(content)
 
-        print(f"Видалено заборонені слова з файлу {input_file}.")
-    except Exception as e:
-        print(f"Під час виключення заборонених слів виникла помилка: {e}")
 
-merged_file = 'merged_files.txt'
-modified_file = 'modified_file.txt'
-search_thread = threading.Thread(target=search_and_merge_files, args=(source_dir, search_word, merged_file))
-search_thread.start()
-search_thread.join()
-exclude_thread = threading.Thread(target=exclude_banned_words, args=(merged_file, banned_words_file, modified_file))
-exclude_thread.start()
-exclude_thread.join()
-print("Операції завершено.")
+
 
