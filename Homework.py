094f2a20126a643e60923a117285619af37f080e
user <andrey.gromov.36912@gmail.com>
2024-05-10 22:11:25 +0300

    Завдання 3

--- Homework.py
+++ Homework.py
@@ -1,42 +1,41 @@
-# Завдання 1
-# При старті додатку запускаються три потоки. Перший
-# потік заповнює список випадковими числами. Два інші потоки
-# очікують на заповнення. Коли перелік заповнений, обидва
-# потоки запускаються. Перший потік знаходить суму елементів
-# списку, другий потік знаходить середнє арифметичне значення
-# у списку. Отриманий список, сума та середнє арифметичне
-# виводяться на екран.
+# Завдання 3
+# Користувач вводить з клавіатури шлях до існуючої та
+# до нової директорії. Після чого запускається потік, який має
+# скопіювати вміст директорії у нове місце. Збережіть структуру
+# директорії. Виведіть статистику виконаних операцій на екран
 
+import os
 import threading
-import random
-numbers = []
-
-def fill_random_numbers(nums):
-    for _ in range(10):
-        nums.append(random.randint(1, 100))
-    print("Отриманий список:", nums)
-def find_sum(nums):
-    sum_result = sum(nums)
-    print("Сума елементів списку:", sum_result)
-def find_average(nums):
-    if nums:
-        average = sum(nums) / len(nums)
-        print("Середнє арифметичне значення у списку:", average)
-    else:
-        print("Список пустий.")
+source_dir = input("Введіть шлях до існуючої директорії: ")
+dest_dir = input("Введіть шлях до нової директорії: ")
 
 
+def copy_directory_contents(source_dir, dest_dir):
+    try:
 
-fill_thread = threading.Thread(target=fill_random_numbers, args=(numbers,))
-sum_thread = threading.Thread(target=find_sum, args=(numbers,))
-average_thread = threading.Thread(target=find_average, args=(numbers,))
-fill_thread.start()
-fill_thread.join()
-sum_thread.start()
-average_thread.start()
-sum_thread.join()
-average_thread.join()
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
 
+
+        for item in os.listdir(source_dir):
+            source_item = os.path.join(source_dir, item)
+            dest_item = os.path.join(dest_dir, item)
+            if os.path.isdir(source_item):
+                copy_directory_contents(source_item, dest_item)
+            else:
+                with open(source_item, 'rb') as source_file:
+                    with open(dest_item, 'wb') as dest_file:
+                        dest_file.write(source_file.read())
+
+        print(f"Директорія {source_dir} успішно скопійована в {dest_dir}")
+    except Exception as e:
+        print(f"Під час копіювання виникла помилка: {e}")
+
+
+copy_thread = threading.Thread(target=copy_directory_contents, args=(source_dir, dest_dir))
+copy_thread.start()
+copy_thread.join()
+print("Копіювання завершено.")
 
 
 
